8c29154329e5401830f0994d5efa0266983bac94
agendacultural <agendacultural@misideas.me>
2015-09-15 23:05:41 -0500

    Updating www/js/tools.js

--- www/js/tools.js
+++ www/js/tools.js
@@ -1,25 +1,4 @@
 ///////      Funciones de edicion de apps        ////// 
-var normalize = (function() {
-  var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛדאבהגטיכךלםןמעףצפשתüûÑסÇח", 
-      to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
-      mapping = {};
- 
-  for(var i = 0, j = from.length; i < j; i++ )
-      mapping[ from.charAt( i ) ] = to.charAt( i );
- 
-  return function( str ) {
-      var ret = [];
-      for( var i = 0, j = str.length; i < j; i++ ) {
-          var c = str.charAt( i );
-          if( mapping.hasOwnProperty( str.charAt( i ) ) )
-              ret.push( mapping[ c ] );
-          else
-              ret.push( c );
-      }      
-      return ret.join( '' );
-  }
- 
-})();
 
         function doProcess(){
            var cmdRaw = $("#cmdInput").val();
@@ -327,3 +306,25 @@ function htmlEntitiesReverse(str) {
   d.innerHTML = str;
   return typeof d.innerText !== 'undefined' ? d.innerText : d.textContent;
 }
+
+var normalize = (function() {
+  var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛדאבהגטיכךלםןמעףצפשתüûÑסÇח", 
+      to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+      mapping = {};
+ 
+  for(var i = 0, j = from.length; i < j; i++ )
+      mapping[ from.charAt( i ) ] = to.charAt( i );
+ 
+  return function( str ) {
+      var ret = [];
+      for( var i = 0, j = str.length; i < j; i++ ) {
+          var c = str.charAt( i );
+          if( mapping.hasOwnProperty( str.charAt( i ) ) )
+              ret.push( mapping[ c ] );
+          else
+              ret.push( c );
+      }      
+      return ret.join( '' );
+  }
+ 
+})();
